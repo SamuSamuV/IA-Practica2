185468d9ec365a32473c5a9f588bbae3a2bd7151
DESKTOP-65SAUST\Alumno <informatica@esne.es>
2025-02-26 20:00:23 +0100

    Codigo com mil errores

--- IA-Practica2/Assets/Scripts/Learn/Pathfinding.cs
+++ IA-Practica2/Assets/Scripts/Learn/Pathfinding.cs
@@ -4,6 +4,9 @@ using UnityEngine;
 
 public class Pathfinding
 {
+    private const int MOVE_STRAIGHT_COST = 10;
+    private const int MOVE_DIAGONAL_COST = 14;
+
     private BoardManager<PathNode> grid;
     private List<PathNode> openList;
     private List<PathNode> closedList;
@@ -16,6 +19,7 @@ public class Pathfinding
     private List<Pathfinding> FindPath(int startX, int startY, int endX, int endY)
     {
         PathNode startNode = grid.GetGridObject(startX, startY);
+        PathNode endNode = grid.GetGridObject(endX, endY);
 
         openList = new List<PathNode> { startNode };
         closedList = new List<PathNode>();
@@ -32,13 +36,40 @@ public class Pathfinding
         }
 
         startNode.gCost = 0;
-        //startNode.gCost =
+        startNode.hCost = CalcuculateDistanceCost(startNode, endNode);
+        startNode.CalculateFCost();
+
+        while(openList.Count > 0)
+        {
+            PathNode currentNode = GetLowestFCostNode(openList);
+            if (currentNode != null)
+            {
+                //Reached final node
+                return CalculatePath(endNode);
+            }
+
+
+        }
     }
 
-    private int CalcuculateDistanceCost(PathNode a, PathNode b)
+    private int CalcuculateDistanceCost(PNode a, PNode b)
     {
         int XDistance = Mathf.Abs(a.x - b.x);
         int YDistance = Mathf.Abs(a.y - b.y);
         int remaining = Mathf.Abs(XDistance - YDistance);
+        return MOVE_DIAGONAL_COST * Mathf.Min(XDistance, YDistance) + MOVE_STRAIGHT_COST * remaining;
+    }
+
+    private PathNode GetLowestFCostNode(List<PathNode> pathNodeList)
+    {
+        PathNode lowestFCostNode = pathNodeList[0];
+        for (int i = 1; i < pathNodeList.Count; i++)
+        {
+            if (pathNodeList[i].fCost < lowestFCostNode.fCost)
+            {
+                lowestFCostNode = pathNodeList[i];
+            }
+        }
+        return lowestFCostNode;
     }
 }
